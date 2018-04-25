0742d822a1197984a4e3a1bfa4f8877ee0b6cc34
17021085 <37399595+17021085@users.noreply.github.com>
2018-04-25 12:29:55 +0700

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -1,8 +1,8 @@
 #include<iostream>
-#include "kmin_console.h"
+#include "console.h"
 #include<ctime>
 #define Dot_ran 254
-#define max 30
+#define max 100
 #define UP 1
 #define DOWN 2
 #define LEFT 3
@@ -10,8 +10,7 @@
 #define Tuong_Tren 1
 #define Tuong_Duoi 25
 #define Tuong_Trai 3
-#define Tuong_Phai 65
-
+#define Tuong_Phai 40
 
 
 using namespace std;
@@ -22,20 +21,22 @@ struct ToaDo
 };
 
 ToaDo ran[max];
-int SoDot = 3;//so dot ban dau 
+int SoDot = 4;//so dot ban dau 
 void KhoiTao();
 void HienthiRan(ToaDo DotCuoi);
 ToaDo Dichuyen(int huong);
 void Batsukien(int &huong);
 ToaDo Hienthimoi();
 bool kiemtraanmoi(ToaDo moi);
+void Themdot();
 bool Gameover();
 void VeTuong();
+void bangdiem();
 void Xulythua();
 
 int main()
 {
-	system("cls");
+	//system("cls");
 	/*for (int i = 0; i < 256; i++) {
 		cout << i <<": " <<(char)i<<endl;
 	}*/
@@ -43,24 +44,36 @@ int main()
 	
 	int x = 0, y = 0;
 	int huong = RIGHT;
-
+	int diem = 0;
+	int Sleeping = 200;
+	
+	
 	VeTuong();
+	bangdiem();
 	ToaDo moi = Hienthimoi();
+	//ToaDo speed =
 	//Vong Lap Game
 	while (1) {
-		//system("cls");
+		
 
 		ToaDo DotCuoi = Dichuyen(huong);
 		Batsukien(huong);
 		HienthiRan( DotCuoi);
 		
 		if (kiemtraanmoi(moi) == true) {
+			Themdot();
+			diem = diem+ Sleeping/10;
+			gotoXY(Tuong_Phai + 8, Tuong_Tren + 2);
+			setTextColor(DARK_GREEN_COLOR);
+			cout << diem;
+			setTextColor(WHITE_COLOR);
 			moi = Hienthimoi();
+		
 		};
 		if(Gameover()== true) 
 			break;
-		Sleep(300);
-
+		Sleep(Sleeping);
+		
 	}
 	Xulythua();
 
@@ -70,9 +83,10 @@ int main()
 
 void KhoiTao() //khoi tao 3 dot ran dau tien
 {
-	ran[0].x = Tuong_Trai + 3;
-	ran[1].x = Tuong_Trai + 2;
-	ran[2].x = Tuong_Trai + 1;
+	
+	ran[0].x = Tuong_Trai + 5;
+	ran[1].x = Tuong_Trai + 4;
+	ran[2].x = Tuong_Trai + 3;
 	ran[0].y = ran[1].y = ran[2].y = Tuong_Tren +3;
 	
 } 
@@ -111,20 +125,20 @@ ToaDo Dichuyen(int huong) {
 void Batsukien(int &huong) 
 {
 	int key = inputKey();
-	if (key == 'w' || key == 'W' || key == KEY_UP)
+	if ( key == KEY_UP && huong != DOWN)
 	{
 		huong = UP;
 	}
-	else if (key == 's'|| key == 'S' || key == KEY_DOWN)
+	else if ( key == KEY_DOWN && huong != UP)
 	{
 		huong = DOWN;
 
 	}
-	else if (key == 'a' || key == 'A' || key == KEY_LEFT)
+	else if ( key == KEY_LEFT && huong != RIGHT)
 	{
 		huong = LEFT;
 	}
-	else if (key == 'd' || key == 'D' || key == KEY_RIGHT)
+	else if ( key == KEY_RIGHT && huong != LEFT)
 	{
 		huong = RIGHT;
 	}
@@ -135,21 +149,16 @@ void VeTuong()
 	for (int i = Tuong_Trai; i <= Tuong_Phai; i++) {
 		gotoXY(i, Tuong_Tren);
 		cout << (char)219;
+		gotoXY(i, Tuong_Duoi);
+		cout << (char)219;
 	}
 	for (int j = Tuong_Tren; j <= Tuong_Duoi; j++) {
 		gotoXY(Tuong_Trai, j); 
-
 		cout << (char)219;
-	}
-	for (int k = Tuong_Trai; k <= Tuong_Phai; k++) {
-		gotoXY(k, Tuong_Duoi);
-		cout << (char)219;
-	}
-	for (int j = Tuong_Tren; j <= Tuong_Duoi; j++) {
 		gotoXY(Tuong_Phai, j);
-
 		cout << (char)219;
 	}
+	
 }
 bool Gameover()
 {
@@ -161,6 +170,12 @@ bool Gameover()
 		return true;
 	if (ran[0].x == Tuong_Phai)
 		return true;
+	for (int i = 1; i <= SoDot - 1; i++) {
+		if (ran[0].x == ran[i].x && ran[0].y == ran[i].y) {
+			return true;
+		}
+		}
+	
 }
 void Xulythua()
 {
@@ -168,17 +183,23 @@ void Xulythua()
 	{
 		Sleep(500);
 		system("cls");
-		cout << "Game over";
+		cout << "Game over "<<endl;
+		
 	}
 }
 ToaDo Hienthimoi()
 {
 	
-	srand(time(NULL));
+	srand(time(0));
+
 	int x = Tuong_Trai + 1 + rand() % (Tuong_Phai - Tuong_Trai - 1);
 	int y = Tuong_Tren + 1 + rand() % (Tuong_Duoi - Tuong_Tren - 1);
-	gotoXY(x, y);
-	cout << (char)3;
+	
+		gotoXY(x, y);
+		setTextColor(RED_COLOR);
+		cout << (char)3;
+		setTextColor(WHITE_COLOR);
+	
 	return ToaDo{ x,y };
 }
 bool kiemtraanmoi( ToaDo moi)
@@ -191,4 +212,34 @@ bool kiemtraanmoi( ToaDo moi)
 	{
 		return false;
 	}
+}
+void Themdot()
+{
+	//ran[SoDot] = ran[SoDot - 1];
+	SoDot++;
+}
+void bangdiem()
+{
+	for (int i = Tuong_Phai + 3; i <= Tuong_Phai + 15; i++) {
+		gotoXY(i, Tuong_Tren);
+		cout << (char)219;
+	}
+	for (int j = Tuong_Tren; j <= Tuong_Duoi - 15; j++) {
+		gotoXY(Tuong_Phai +3, j);
+
+		cout << (char)219;
+	}
+	for (int k = Tuong_Phai+ 3; k <= Tuong_Phai + 15; k++) {
+		gotoXY(k, Tuong_Duoi-15);
+		cout << (char)219;
+	}
+	for (int l = Tuong_Tren; l <= Tuong_Duoi - 15; l++) {
+		gotoXY(Tuong_Phai+15, l);
+
+		cout << (char)219;
+	}
+	gotoXY(Tuong_Phai + 4, Tuong_Tren + 2);
+	setTextColor(RED_COLOR);
+	cout << (char)3 << ": " ;
+	setTextColor(WHITE_COLOR);
 }
